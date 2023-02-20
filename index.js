5a7100a13bb3dc4994ec6390a2692b9a05a623b3
Gage Sorrell <1-root@users.noreply.git.vsc.company>
2023-02-19 20:57:11 -0500

    Is done now.

--- index.js
+++ index.js
@@ -1,92 +1,62 @@
 const { Client } = require('@notionhq/client');
 
-// Initialize a new Notion API client with an integration token
-const notion = new Client({ auth: 'secret_TxZiy9dCzzdOouokBZD5Uty7eOVdAdvQVoLiwRYraYA' });
+exports.handler = async (event, context) => {
+    // Get the Supplement and Dose values from the event
+    //   const { Supplement, Dose } = JSON.parse(event.body);
+    const RelevantBody = JSON.parse(event.body);
+    const SupplementInput = RelevantBody.Supplement;
+    const Dose = Number(RelevantBody.Dose);
 
-// Define the database ID for the page you want to create
-const databaseId = '49a66d6b23b84184b9b0c93258c7a756';
+    const now = new Date();
 
-// Define the title of the new page
-const pageTitle = 'Shopping List 2.19';
+    // Get the time zone offset for Fort Wayne, Indiana
+    const tzOffset = -240; // UTC-4
 
-// Define the list of items with their corresponding quantities, replacing tabs with ", "
-const items = [
-  'Old-fashioned oats, 7 cups',
-  'Natural peanut butter, 1 jar',
-  'Almond milk (no sugar added), 112 fl oz (7 containers)',
-  'Chicken breast, 5.25 lbs',
-  'Brown rice, 7 cups',
-  'Green beans, 4 cups',
-  'Brussels sprouts, 2 cups',
-  'Carrots, 4 cups',
-  'Common mushrooms, 2 cups',
-  'Spinach, 2 cups',
-  'Capsicum (any flavor), 4 cups',
-  'Sweet potato, 3 cups',
-  'Red onion, 1.25 cups',
-  'Garlic, 10 cloves',
-  'Shallot, 1',
-  'Broccoli, 2 cups',
-  'Mung bean sprouts, 2 cups',
-  'Cauliflower, 2 cups',
-  'Asparagus, 2 cups',
-  'Zucchini, 3 cups',
-  'Poblano peppers, 2 cups',
-  'Black beans, 2 cups',
-  'Greek yogurt, 2 cups',
-  'Mixed berries, 1 cup',
-  'Honey, 1/4 cup',
-  'Cottage cheese, 1 cup',
-  'Mixed nuts, 2/3 cup',
-  'Apple, 1',
-  'Almond butter, 2 Tbsp',
-  'Hummus, 1/2 cup',
-  'Baby carrots, 1 cup',
-  'Protein bar, 1',
-  'Protein shake (whey protein, almond milk), 2 servings'
-];
+    // Add the offset to the current time
+    const fwTime = new Date(now.getTime() + tzOffset * 60 * 1000);
 
-// Create a new page in the specified database with to-do blocks containing the shopping list items
-async function createPage() {
-  try {
-    // Create a new page with the specified title in the root of your Notion account
-    const newPage = await notion.pages.create({
-      parent: { database_id: databaseId },
-      properties: {
-        title: [{ text: { content: pageTitle } }]
-      }
-    });
+    // Format the time as an ISO string
+    const isoString = fwTime.toISOString();
 
-    // Define the new page ID
-    const newPageId = newPage.id;
 
-    // Loop through the list of items and add each one as a to-do block on the new page
-    for (const item of items) {
-      await notion.blocks.children.append({
-        block_id: newPageId,
-        children: [
-          {
-            object: 'block',
-            type: 'to_do',
-            to_do: {
-              rich_text: [
-                {
-                  type: 'text',
-                  text: {
-                    content: item
-                  }
-                }
-              ]
+    // Initialize the Notion client
+    const notion = new Client({ auth: "secret_TxZiy9dCzzdOouokBZD5Uty7eOVdAdvQVoLiwRYraYA" });
+
+    // Find the database with the Select property
+    const databaseId = "aee857ac88c543f38d8d4971169e36c9";
+    const database = await notion.databases.retrieve({ database_id: databaseId });
+
+    // Find the option corresponding to the provided supplement value
+    console.log(database.properties);
+    const supplementOption = database.properties.Supplement.select.options.find(option => option.name === SupplementInput);
+
+    console.log("SupplementOption ID was " + supplementOption.id);
+    // Create a new page in the database with the specified properties
+    const newPage = {
+        "Supplement":
+        {
+            "select":
+            {
+                "name": SupplementInput
             }
-          }
-        ]
-      });
-    }
+        },
+        "Dose (mg)": {
+            number: Dose
+        },
+        Time:
+        {
+            date:
+            {
+                start: isoString
+            }
+        },
+        "Name": { title: [{ text: { content: "" } }] }
+    };
+    await notion.pages.create({ parent: { database_id: databaseId }, properties: newPage });
 
-    console.log(`New page "${pageTitle}" created successfully with a to-do list!`);
-  } catch (error) {
-    console.error(error.body);
-  }
-}
-
-createPage();
+    // Return a success response
+    return {
+        statusCode: 200,
+        body: "Page created successfully"
+    };
+};
